338cba60dfa26416c83701de5b1975d2a9c76fc4
ginsm <matt@mgin.me>
2019-12-31 11:47:42 -0500

    Add an onNoCommand function for simplecmds

--- src/interface/options.js
+++ src/interface/options.js
@@ -1,11 +1,23 @@
 // -------------------------------------------- //
+// SECTION - Module Imports
+// -------------------------------------------- //
+
+// Internal modules
+const view = require('../view');
+
+
+// -------------------------------------------- //
 // SECTION - Program Options
 // -------------------------------------------- //
 
 const Options = {
+  description: 'Create and manage multiple task lists',
   defaults: {
     rules: '<number,string>',
     amount: 1,
+  },
+  onNoCommand: function() {
+    view.displayTasks();
   },
 };
 
